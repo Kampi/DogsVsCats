1e0ddae8fec66e6cded171a7bab44f7e96844c84
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2019-07-23 08:37:26 +0200

    Disable output, add creation of logdir

--- Convert.bat
+++ Convert.bat
@@ -1,2 +1,5 @@
+@echo off
+
+if exist tensorflow_log_dir rd /s /q tensorflow_log_dir
 python .\import_pb_to_tensorboard.py --model_dir data/output/Model.pb --log_dir=tensorflow_log_dir
 tensorboard --logdir=tensorflow_log_dir
